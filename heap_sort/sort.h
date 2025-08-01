55c823fc90b09fb7c30ec2ab3826dec556d235da
TonyHolby <tonynemoutheholberton@gmail.com>
2025-08-01 13:25:35 +0200

    Fix code in the header file

--- heap_sort/sort.h
+++ heap_sort/sort.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 
 void print_array(const int *array, size_t size);
+void heapify(int arr[], size_t size, int n, int i);
 void heap_sort(int *array, size_t size);
 
 #endif /* SORT_H */
