5c37dad2c74546f2bc5c37f5c029b016015fd0a6
TonyHolby <tonynemoutheholberton@gmail.com>
2025-08-31 11:11:58 +0200

    Add the header file

---
+++ infinite_multiplication/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(int argc, char *argv[]);
+int _putchar(char c);
+
+#endif /* HOLBERTON_H */
