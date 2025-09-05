3595810caabed24bfeb97e4237227abc564dc94a
TonyHolby <tonynemoutheholberton@gmail.com>
2025-09-05 11:22:31 +0200

    Write code in 0-rotate_2d_matrix.py

---
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" A function that rotates a 2D matrix 90 degrees clockwise. """
+
+
+def rotate_2d_matrix(matrix):
+    """ Rotates a 2D matrix 90 degrees clockwise. """
+    matrix[:] = [list(row)[::-1] for row in zip(*matrix)]
