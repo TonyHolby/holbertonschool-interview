35d36b466c568a4765457e8db874236d054ef09b
TonyHolby <tonynemoutheholberton@gmail.com>
2025-09-19 22:15:45 +0200

    Fix the header file

--- merge_sort/sort.h
+++ merge_sort/sort.h
@@ -8,6 +8,5 @@ void print_array(const int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void merge(int *array, int *temp, size_t left, size_t mid, size_t right);
 void merge_sort_recursive(int *array, int *temp, size_t left, size_t right);
-void print_subarray(int *array, size_t start, size_t end);
 
 #endif /* SORT_H */
