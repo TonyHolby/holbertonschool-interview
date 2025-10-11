9b94b50c89d2e94159936479d56a4e5f0d1932f8
TonyHolby <tonynemoutheholberton@gmail.com>
2025-10-11 10:46:05 +0200

    Fix betty style in 0-radix_sort.c

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -8,16 +8,16 @@
  */
 int get_max(int *array, size_t size)
 {
-    int max = array[0];
-    size_t index;
+	int max = array[0];
+	size_t index;
 
-    for (index = 1; index < size; index++)
-    {
-        if (array[index] > max)
-            max = array[index];
-    }
+	for (index = 1; index < size; index++)
+	{
+		if (array[index] > max)
+			max = array[index];
+	}
 
-    return (max);
+	return (max);
 }
 
 /**
@@ -29,33 +29,33 @@ int get_max(int *array, size_t size)
  */
 void counting_sort_radix(int *array, size_t size, int digit_position)
 {
-    int *sorted_array = malloc(size * sizeof(int));
-    int count[10] = {0};
-    size_t index;
+	int *sorted_array = malloc(size * sizeof(int));
+	int count[10] = {0};
+	size_t index;
 
-    if (!sorted_array)
-        return;
+	if (!sorted_array)
+		return;
 
-    for (index = 0; index < size; index++)
-        count[(array[index] / digit_position) % 10]++;
+	for (index = 0; index < size; index++)
+		count[(array[index] / digit_position) % 10]++;
 
-    for (index = 1; index < 10; index++)
-        count[index] += count[index - 1];
+	for (index = 1; index < 10; index++)
+		count[index] += count[index - 1];
 
-    for (index = size; index > 0; index--)
-    {
-        int current_value = array[index - 1];
-        int digit = (current_value / digit_position) % 10;
-        int position = count[digit] - 1;
+	for (index = size; index > 0; index--)
+	{
+		int current_value = array[index - 1];
+		int digit = (current_value / digit_position) % 10;
+		int position = count[digit] - 1;
 
-        sorted_array[position] = current_value;
-        count[digit]--;
-    }
+		sorted_array[position] = current_value;
+		count[digit]--;
+	}
 
-    for (index = 0; index < size; index++)
-        array[index] = sorted_array[index];
+	for (index = 0; index < size; index++)
+		array[index] = sorted_array[index];
 
-    free(sorted_array);
+	free(sorted_array);
 }
 
 /**
@@ -66,16 +66,16 @@ void counting_sort_radix(int *array, size_t size, int digit_position)
  */
 void radix_sort(int *array, size_t size)
 {
-    int max, exp;
+	int max, exp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    max = get_max(array, size);
+	max = get_max(array, size);
 
-    for (exp = 1; max / exp > 0; exp *= 10)
-    {
-        counting_sort_radix(array, size, exp);
-        print_array(array, size);
-    }
+	for (exp = 1; max / exp > 0; exp *= 10)
+	{
+		counting_sort_radix(array, size, exp);
+		print_array(array, size);
+	}
 }
